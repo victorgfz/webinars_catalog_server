3c945edf2ab8a40d418d25dce333b82cee9c4fa4
Victor Gomes Ferraz <victor.sjb.2002@gmail.com>
2025-10-01 18:11:59 -0300

    build: update configs for vercel

--- src/index.ts
+++ src/index.ts
@@ -7,7 +7,6 @@ import webinarRouter from "./routes/webinar.routes"
 import filterRouter from "./routes/filter.routes"
 
 const app = express();
-const port = 8080;
 
 const corsOptions = {
     origin: process.env.CORS_ORIGIN,
@@ -18,14 +17,22 @@ app.use(cors(corsOptions));
 app.use(cookieParser());
 app.use(express.json());
 
-
 app.use("/auth", authRouter)
 app.use("/user", userRouter)
 app.use("/webinar", webinarRouter)
 app.use("/filter", filterRouter)
 
 
-
-app.listen(port, () => {
-    console.log(`Running on port ${port}`)
+app.get("/", (req: Request, res: Response) => {
+    res.json({ message: "API is running on Vercel!" })
 })
+
+
+if (process.env.NODE_ENV !== 'production') {
+    const port = process.env.PORT || 8080;
+    app.listen(port, () => {
+        console.log(`Running on port ${port}`)
+    })
+}
+
+export default app;
