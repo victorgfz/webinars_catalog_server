9dd7c632d79f6a3e3fa91e58be6780773efa83ba
Victor Gomes Ferraz <victor.sjb.2002@gmail.com>
2025-10-01 18:51:14 -0300

    fix: cookies for production

--- src/controllers/auth.controllers.ts
+++ src/controllers/auth.controllers.ts
@@ -20,19 +20,26 @@ export async function loginUser(req: Request, res: Response) {
             return res.status(401).json({ message: "Invalid credentials" })
         }
 
+        const isProduction = process.env.NODE_ENV === 'production'
+
         res.status(200).cookie("token", loginToken, {
             httpOnly: true,
-            secure: false,
-            sameSite: "strict",
+            secure: isProduction,
+            sameSite: isProduction ? "none" : "strict",
             maxAge: 1000 * 60 * 60 * 24
         }).json({ message: "Login successful!" })
     } catch (error) {
         res.status(500).json({ message: "Internal server error" })
     }
-
 }
 
 export function logoutUser(req: Request, res: Response) {
-    res.clearCookie('token', { httpOnly: true, secure: false, sameSite: "strict" });
+    const isProduction = process.env.NODE_ENV === 'production'
+
+    res.clearCookie('token', {
+        httpOnly: true,
+        secure: isProduction,
+        sameSite: isProduction ? "none" : "strict"
+    });
     return res.status(200).json({ ok: true });
 }
